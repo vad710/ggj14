d8e9624d41400dfa707e6b8867564ca20ccc2647
vad710 <vinny.dasilva@gmail.com>
2014-01-24 23:40:20 -0500

    Added SceneBuilder and Gamestate

---
+++ WitnessProtection/Assets/GameState.cs
@@ -0,0 +1,22 @@
+﻿namespace Assets
+{
+    public class GameState
+    {
+        private GameState()
+        {
+            //enforces instantiation via the singleton
+        }
+
+        private static GameState _instance;
+
+        public static GameState Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new GameState();
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ WitnessProtection/Assets/SceneBuilder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class SceneBuilder : MonoBehaviour 
+    {
+        // Use this for initialization
+        void Start () 
+        {
+	
+        }
+	
+        // Update is called once per frame
+        void Update () 
+        {
+	
+        }
+    }
+}
