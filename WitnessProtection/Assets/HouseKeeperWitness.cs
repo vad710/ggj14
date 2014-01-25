a646779256ec02d973fd8095478e03e1c2ae9204
vad710 <vinny.dasilva@gmail.com>
2014-01-24 23:23:32 -0500

    Added HouseKeeperWitness

---
+++ WitnessProtection/Assets/HouseKeeperWitness.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class HouseKeeperWitness : IWitness
+    {
+        public HouseKeeperWitness()
+        {
+            this.SawApple = true;
+            this.ApplePosition = new Vector2(20,30);
+        }
+
+        public bool SawApple { get; private set; }
+        public Vector2 ApplePosition { get; private set; }
+    }
+}
