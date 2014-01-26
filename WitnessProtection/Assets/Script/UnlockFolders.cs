8d0312e333ca85ea38b5440cc329f6b2434d1eab
vad710 <vinny.dasilva@gmail.com>
2014-01-26 13:05:00 -0500

    Folder Unlocking Logic

---
+++ WitnessProtection/Assets/Script/UnlockFolders.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Script;
+using UnityEngine;
+using System.Collections;
+
+public class UnlockFolders : MonoBehaviour
+{
+
+    public GameObject CalebFolder;
+    public GameObject EliFolder;
+    public GameObject DannyFolder;
+
+	// Use this for initialization
+	public void Start ()
+	{
+	    var gamestate = GameState.Instance();
+
+	    CalebFolder.SetActive(gamestate.CalebUnlocked);
+        EliFolder.SetActive(gamestate.EliUnlocked);
+        DannyFolder.SetActive(gamestate.DannyUnlocked);
+	}
+}
